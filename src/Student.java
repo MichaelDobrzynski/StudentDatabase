f4385e54095f7ff56b640a866bf52787350091df
midoit01 <midoit01@hs-esslingen.de>
2022-10-04 14:43:44 +0200

    Student class outline

---
+++ src/Student.java
@@ -0,0 +1,27 @@
+public class Student {
+    private int studentID;
+    private String firstName;
+    private String lastName;
+    private String year;
+    private int balance;
+    private String courses;
+    private int costOfCourse = 600;
+
+    //constructor prompts user to enter first name, last name, and year
+    public Student(String firstName, String lastName, String year) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.year = year;
+    }
+
+    //generate an ID
+
+    //enroll in courses
+
+    //view balance
+
+    //pay tuition
+
+    //view status
+
+}
--- src/StudentDbApp.java
+++ src/StudentDbApp.java
@@ -1,6 +1,12 @@
+import java.io.BufferedReader;
+
 public class StudentDbApp {
 
     public static void main(String[] args) {
+
+        //Ask how many students will be added to the database
+
+        //Create n number of new students
 
     }
 }
