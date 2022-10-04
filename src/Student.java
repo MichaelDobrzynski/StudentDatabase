d018da41d763232737f612a1f1d44db6c53adcf3
midoit01 <midoit01@hs-esslingen.de>
2022-10-04 15:05:13 +0200

    create unique student id

--- src/Student.java
+++ src/Student.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
 
 public class Student {
-    private int studentID;
+    private String studentID;
     private String firstName;
     private String lastName;
     private int year;
     private int balance;
     private String courses;
-    private int costOfCourse = 600;
+
+    //reminder: static means that value or property is unspecific to object, but rather to entire class
+    private static int costOfCourse = 600;
+
+    private static int id = 1000;
 
     //constructor prompts user to enter first name, last name, and year
     public Student() {
@@ -20,12 +24,22 @@ public class Student {
         this.lastName = in.nextLine();
 
         System.out.println("Student undergraduate years: \n 1. Freshman \n 2. Sophomore \n 3. Junior \n 4. Senior \n Choose one: ");
+        //implement code to only allow for numbers 1 - 4 to be entered
         this.year = in.nextInt();
-        System.out.println(firstName + " " + lastName + ": " + year);
+
+        setStudentID();
+
+        System.out.println(firstName + " " + lastName + ", " + year + ", " + studentID);
 
     }
 
     //generate an ID
+    private void setStudentID() {
+        //instantiates static var id for each new student created
+        id++;
+        // Grade level + ID
+        this.studentID = year + "" + id;
+    }
 
     //enroll in courses
 
