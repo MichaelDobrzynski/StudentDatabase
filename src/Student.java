5a97b768d503424975a7dfe70cfcc4cad5e2270e
midoit01 <midoit01@hs-esslingen.de>
2022-10-11 14:25:18 +0200

    added setStudentYear function

--- src/Student.java
+++ src/Student.java
@@ -11,6 +11,7 @@ public class Student {
     //reminder: static means that value or property is unspecific to object, but rather to entire class
     private static int costOfCourse = 600;
 
+    //ID made up of five integers, the first being the students year and the following four with an increment of the static var which is 1000 to start
     private static int id = 1000;
 
     //constructor prompts user to enter first name, last name, and year
@@ -24,9 +25,11 @@ public class Student {
         this.lastName = in.nextLine();
 
         System.out.println("Student undergraduate years: \n 1. Freshman \n 2. Sophomore \n 3. Junior \n 4. Senior \n Choose one: ");
-        //implement code to only allow for numbers 1 - 4 to be entered
-        this.year = in.nextInt();
 
+        //implemented code to only allow for numbers 1 - 4 to be entered
+        setStudentYear();
+
+        //allows for easy incrementation of unique student ID's
         setStudentID();
     }
 
@@ -36,6 +39,18 @@ public class Student {
         id++;
         // Grade level + ID
         this.studentID = year + "" + id;
+    }
+
+    // function to set student year using if-else statement to allow for valid inputs only, some lag still present during running of application
+    private void setStudentYear() {
+        Scanner in = new Scanner(System.in);
+        if(in.nextInt() > 0 &&   in.nextInt() < 5) {
+            this.year = in.nextInt();
+            System.out.println("Student year is: " + year);
+        } else {
+            System.out.println("Invalid input. Please try again.");
+            setStudentYear();
+        }
     }
 
     //enroll in courses
